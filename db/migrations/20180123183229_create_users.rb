3f8612b4036aa05b0a7e3982611673b76c37d2d0
f22fef36 <f22fef36@protonmail.com>
2018-01-23 16:04:12 -0500

    Define the User attributes

--- db/migrations/20180123183229_create_users.rb
+++ db/migrations/20180123183229_create_users.rb
@@ -3,6 +3,11 @@ Hanami::Model.migration do
     create_table :users do
       primary_key :id
 
+      column :email,      String,   null: false, unique: true
+      column :first_name, String,   null: false
+      column :last_name,  String,   null: false
+      column :password,   String,   null: false
+
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
