68bbbfcac80adde286687b0184c577722c5922b5
f22fef36 <f22fef36@protonmail.com>
2018-01-23 16:54:54 -0500

    Add basic test to User spec

--- spec/authdemo/entities/user_spec.rb
+++ spec/authdemo/entities/user_spec.rb
@@ -1,5 +1,17 @@
 require_relative '../../spec_helper'
 
 describe User do
-  # place your tests here
+  let(:user_attributes) do
+    Hash[
+      email: 'first.last@example.org',
+      first_name: 'First',
+      last_name: 'Last',
+      password: 'password',
+    ]
+  end
+
+  it 'can be initialized with attributes' do
+    user = User.new(user_attributes)
+    user.email.must_equal 'first.last@example.org'
+  end
 end
